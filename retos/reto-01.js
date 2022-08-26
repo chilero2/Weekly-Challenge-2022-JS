7b179b6d12c36bbc250fd7afc5685c06ee899712
Emilio <emilioblasco@icloud.com>
2022-08-26 18:40:43 +0200

    Commit reto-01

---
+++ retos/reto-01.js
@@ -0,0 +1,32 @@
+/*
+ * Reto #1
+ * ¿ES UN ANAGRAMA?
+ * Fecha publicación enunciado: 03/01/22
+ * Fecha publicación resolución: 10/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+const esAnagrama = (word1, word2) => {
+    if(word1.toLowerCase() === word2.toLowerCase() || word1.legth != word2.legth) return false
+    const mat1 = Array.from(word1.toLowerCase())
+    const mat2 = Array.from(word2.toLowerCase())
+    return mat1.sort().join('') === mat2.sort().join('')
+}
+
+const word1 = 'castillo'
+const word2 = 'ollicast'
+
+// console.log(esAnagrama(word1, word2))
+console.log(`${word1} y ${word2} ${esAnagrama(word1, word2) ? 'son' : 'no son'} un anagrama`)
